bd86567c82368d1fbaff78cb8d9b1d32226babc5
Leticia Aldaco <leticiaaldaco@leticias-mbp.lan>
2021-04-24 20:44:47 -0500

    added psuedo code and work hours array

---
+++ assets/js/script.js
@@ -0,0 +1,30 @@
+// Add current date 
+
+// Create Array that contains the times needed
+var workHours= [9, 10, 11, 12, 13, 14, 15, 16, 17];
+function displayBlock (){
+// Add time blocks 
+
+/*
+   <!-- <div class="row time-block">
+      <div class="col-1 hour"> 9am</div>
+      <textarea class="col-10 description"></textarea>
+      <button class="col-1 saveBtn">
+        <i class="fas fa-save"></i>
+      </button>
+
+    </div>
+*/
+// Create a for loop, loops thorugh length of Work Hours and dinamically create time blocks
+
+// Add past,present, future classes
+}
+
+// Add text box to enter event 
+
+// Create a button to save event
+
+// On click save button capture text area value and button id
+
+// Create funtion to save tasks (set item)
+// Create function to grab tasks from local storage (get item)
